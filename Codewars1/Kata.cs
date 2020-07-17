ff55d981f08700d60a4e73408044e5d0614f326e
giselleLago <gisellelago25994@gmail.com>
2020-07-17 11:17:57 +0200

    Codewars 1

---
+++ Codewars1/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codewars1
+{
+    class Kata
+    {
+        public string AlphabetPosition(string text)
+        {
+            return string.Join(" ", text.ToUpper().Where(char.IsLetter).Select(x => x - 64));
+        }
+    }
+}
---
+++ Codewars1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Codewars1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            Console.WriteLine(a.AlphabetPosition("o' clock."));
+            Console.ReadKey();
+        }
+        
+    }
+}
